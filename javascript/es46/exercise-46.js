baff7e963930ca38bc53b59e5e9c7e9a121e9c60
Angela Balsamo <cuoreangela@hotmail.it>
2022-04-01 22:06:50 +0200

    esercizio47 v1

---
+++ javascript/es46/exercise-46.js
@@ -0,0 +1,14 @@
+function isAdult(person) {
+  return person.age >= 18;
+}
+
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+const {person.age <= 18} = adult
+
+console.log(isAdult(person));
