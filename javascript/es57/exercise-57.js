d23a74de44d0cc86dd61b28657d52e99de6f9654
Angela Balsamo <cuoreangela@hotmail.it>
2022-04-07 00:17:30 +0200

    esercizio57 v1

---
+++ javascript/es57/exercise-57.js
@@ -0,0 +1,30 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+function fetchPersonById(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(persons.filter((person) => person.id === id))
+    }, 1000)
+  })
+}
+
+fetchPersonById(2).then((returnedResolve) => console.log(returnedResolve));
