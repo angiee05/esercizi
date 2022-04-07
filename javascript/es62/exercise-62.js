bcc51a1b9048db235c15dcbf1179d818dd58b0ea
Angela Balsamo <cuoreangela@hotmail.it>
2022-04-07 17:21:11 +0200

    esercizio62 v1

---
+++ javascript/es62/exercise-62.js
@@ -0,0 +1,52 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const person = persons.find(item => item.id === id);
+
+      if (person) {
+        return resolve(JSON.stringify(person));
+      }
+
+      return reject(`Person with id: ${id} doesn't exist`);
+    }, 1000);
+  });
+}
+
+//fetchPersonById(2)
+//  .then((personJson) => JSON.parse(personJson))
+//  .then((person) => console.log(person))
+//  .catch((err) => console.error(err));
+
+async function getResponse() {
+  try {
+      const response = await fetchPersonById(2);
+      console.log(JSON.parse(response));
+  } catch(err) {
+      console.log(err)
+  } finally {
+      console.log("Completato")
+  }
+}
+
+getResponse()
